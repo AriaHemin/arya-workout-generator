2ac1977a27a581a63f588493a73b9408651c1f35
Aria Hemin <ariahemin@gmail.com>
2023-01-13 16:21:07 +0300

    added exercise api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(()=>{
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '453537aa17mshecaf242ef67c33ap145b9bjsn75fc63b806fc',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  },[])
+
   return (
     <div className="App">
       hello
